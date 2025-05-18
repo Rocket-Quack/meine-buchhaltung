d72df5a5fdd0281ed42bb9829f3414b8eb45f2a7
Rocket-Quack <202538874+Rocket-Quack@users.noreply.github.com>
2025-05-18 19:44:16 +0200

    Fix typos in package and theme names
    Corrected "MeineBuchaltung" to "MeineBuchhaltung" in various XML files and Java/Kotlin import statements.
    This ensures consistency and corrects a typo in the application's theme and package references.

--- app/src/main/java/com/rocketquackit/meinebuchhaltung/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rocketquackit/meinebuchhaltung/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.rocketquackit.meinebuchaltung.databinding.FragmentHomeBinding
+import com.rocketquackit.meinebuchhaltung.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 

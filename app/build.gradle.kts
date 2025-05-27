f69ee8ee5211b52e2e65643d3a431af9ff7dd1b0
Rocket-Quack <202538874+Rocket-Quack@users.noreply.github.com>
2025-05-27 15:24:15 +0200

    Changed App Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rocketquackit.meinebuchhaltung"
         minSdk = 30
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

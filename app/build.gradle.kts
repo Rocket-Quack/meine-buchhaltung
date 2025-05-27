ffb2cb743a0bf4300fe480efc5d83fc7431137c2
Rocket-Quack <202538874+Rocket-Quack@users.noreply.github.com>
2025-05-27 13:00:27 +0200

    build.gradle.kts uses keystore for signature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: project.findProperty("android.injected.signing.store.file") as String)
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: project.findProperty("android.injected.signing.store.password") as String)
+            keyAlias = System.getenv("KEY_ALIAS") ?: project.findProperty("android.injected.signing.key.alias") as String)
+            keyPassword = System.getenv("KEY_PASSWORD") ?: project.findProperty("android.injected.signing.key.password") as String)
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
